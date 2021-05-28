16a758ae48b2c726552bc88e359bffbf636f913d
Maureen <haddadi.maureen@gmail.com>
2021-05-28 20:29:51 +0200

    dynamic data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,42 @@
+let photgraphersList = document.querySelector('.photografer_list')
 function getData() {
-    const myPromise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         fetch('/assets/js/data.json')
             .then((response) => {
                 return response.json()
             })
             .then((data) => {
                 resolve(data)
-                const photo = data.photographers
-                photo.forEach(e => {
-                    console.log(e)
-                })
             })
             .catch((err) => {
                 reject(err)
             })
     })
 
-    return myPromise
-
 }
 
 getData().then((data) => {
-    console.log(data)
+    const photographers = data.photographers
+    photographers.forEach(e => {
+        photgraphersList.innerHTML += `
+       <div class='photografer_card'>
+        <a class="photografer_card__link" href="#">
+            <img class='photografer_card__img' src="assets/img/${e.portrait}" alt='photo'> 
+            <h2 class="photografer_card__title">${e.name}</h2>
+        </a>
+       </div> `
+
+        // photgraphersList.innerHTML += "<div class='photografer_card'><a class=\"photografer_card__link\" href=\"#\"><img class='photografer_card__img' src=\"assets/img/" + e.portrait + "\"" + "alt='photo'> <h2 class=\"photografer_card__title\">" + e.name + "</h2></a></div>"
+
+        document.body.appendChild(photgraphersList);
+    })
+    let allTags = []
+    photographers.forEach(t => {
+        allTags.push(...t.tags)
+    })
+    allTags = [...new Set(allTags)]
+    console.log(allTags)
+
 })
 
+{/* */ }
